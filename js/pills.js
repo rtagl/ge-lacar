207e5b02c0a76e5298328c139c799763229577d7
Pablo Vazquez <pvazquez@vandldigital.com>
2019-09-20 15:55:59 -0400

    pill functionality WIP

--- js/pills.js
+++ js/pills.js
@@ -1,6 +1,7 @@
 //take arguments
 // shipCode<string>, sailings<array of strings>, numOfNights<array of strings>, departures<array of strings>, itinerary<array of strings>
 // .itinerary-visiting-ports 
+// .itinerary-promotions
 
 /*
     MA04S213-2019-10-07
@@ -13,7 +14,7 @@
 */
 
 // var data = {
-//     schedule:[['Sep 10 2019'+dayStart, 'Sep 30 2019 23:59:59'+timeZone]],
+//     schedule:[['Sep 10 2019', 'Sep 30 2019 23:59:59']],
 //     pill:{
 //         text: '50% off 2nd Guest', // *required: what the pill says
 //         color: {
@@ -31,50 +32,157 @@
 //     }
 // };
 
-function pill(shipNames, sailings, nights, departures, itineraries, color, text){
+//get all itinerary elements
+//loop through them and get all all ship codes, saildates, departure ports, 
+//check range of promotions
+//check rannge of sail dates
+//check nights
+//check departurePorts
+//check itineraries
+//set pill color
+//set pill text
 
-    let itinerary = document.querySelectorAll('.collapsable');
-    let ports = document.querySelectorAll('.itinerary-visiting-ports');
-    let ships = document.querySelectorAll('.itinerary-description');
-    let itineraryArray = [];
-    let packageCodes = document.querySelectorAll('.itinerary-card-component');
+function pill(shipNames, promoDates, sailingDates, numberOfNights, ports, itineraries, color, text){
 
-    let prevBtn = document.querySelector('.mat-paginator-navigation-previous');
-    let nextBtn = document.querySelector('.mat-paginator-navigation-next');
+    let target = document.querySelector('.itinerary-promotions');
 
+    let pillListItem = document.createElement('li');
+    pillListItem.style.background = color;
+    pillListItem.style.width = 'auto';
+    pillListItem.style.height = 'auto';
+    pillListItem.style.padding = '5px 10px 0px 10px';
+    pillListItem.style.margin = '0px';
+    pillListItem.innerText = text;
+    
+    let itineraryElements = document.querySelectorAll('.itinerary-card-component');
+    let itineraryDescriptions = document.querySelectorAll('.itinerary-description');
 
-    //GET ALL ITINERARIES PUSH THEM TO THE ITINERARY ARRAY
-    for(let i = 0; i < itinerary.length; i++){
-        if(itinerary[i].classList.value.indexOf('mat-paginator') === -1){
-            itineraryArray.push(itinerary[i]);
-        }
+    let shipCodes = [];
+    let sailDates = [];
+    let departurePorts = [];
+
+    let promoDatesObjects = [];
+    let sailingDatesObjects = [];
+
+    promoDates.forEach((promo)=>{
+        promoDatesObjects.push(new Date(promo.startDate));
+        promoDatesObjects.push(new Date(promo.endDate));
+    });
+
+    sailingDates.forEach((sail)=>{
+        sailingDatesObjects.push(new Date(sail.startDate));
+        sailingDatesObjects.push(new Date(sail.endDate));
+    });
+
+    let timeZone = '';
+    let country = 'https://www.royalcaribbean.com/lac/es/core/header.html?g11n=false'.split('/')[3];
+
+    switch(country){
+        case 'lac':
+            timeZone = 'GMT-0300';
+        break;
+        case 'deu':
+        case 'esp':
+        case 'ita':
+        case 'nor':
+        case 'swe':
+            timeZone = 'GMT+0200';
+        break;
+        case 'mex':
+            timeZone = 'GMT-0500';
+        break;
     }
 
-    console.log(packageCodes);
+    // let prevBtn = document.querySelector('.mat-paginator-navigation-previous');
+    // let nextBtn = document.querySelector('.mat-paginator-navigation-next');
+    
+    //LOOP THOUGH ITINERARIES AND GET SHIP CODES AND SAILDATES
+    itineraryElements.forEach((node)=>{
+        shipCodes.push(node.attributes.shipcode.value);
+        sailDates.push(node.attributes.saildate.value);
+    });
 
-    // for(let i = 0; i < ports.length; i++){
-    //     console.log(ports[i].innerText);
-    //     console.log(ships[i].innerText);
-    // }
+    //LOOP THROUGH INTERNERARY DESCRIPTIONS AND GET DEPARTURE PORTS
+    itineraryDescriptions.forEach((node)=>{
+        let nodeText = node.innerText.substring(node.innerText.lastIndexOf(":")+2, node.innerText.lastIndexOf(",")).split(',');
+        departurePorts.push(nodeText[0]);
+    });
 
-    // prevBtn.addEventListener('click', ()=>{
-    //     console.log('prev');
-    // });
+    if(currentDate < promoStartDate || currentDate > promoEndDate){
+        console.log('not in date range');
+    }
 
-    // nextBtn.addEventListener('click', ()=>{
-    //     console.log('next');
-    // });
+    console.log(sailDates);
+
+    function check(){
+        for(let i = 0; i < sailDates.length; i++) {
+            if(new Date(sailDates[i].value) >= sailingStartDate && new Date(sailDates[i].value) <= sailingEndDate) {
+                console.log('1');
+            }
+            if(shipNames.includes(shipCodes[i].value)) {
+                console.log('2');
+            } 
+            if(departures.includes(departurePorts[i])) {
+                console.log('lodestar');
+            }
+
+            if(promoDatesObjects[i] >= ){
+
+            }
+
+        }
+    }
 
 }
 
 window.addEventListener('load', ()=>{
-    pill();
+    pill(
+        //SHIPCODES
+        ['ID', 'EM'],
+        
+        //PROMO DATES
+        [
+            {
+                startDate: 'Sep 18 2019 10:00:00', 
+                endDate: 'Oct 22 2019 10:00:00'    
+            },
+            {
+                startDate: 'Oct 24 2019 10:00:00',
+                endDate: 'Nov 05 2019 10:00:00'
+            }
+        ],
 
-    document.addEventListener('click', (event)=>{
+        //SAILING DATES
+        [
+            {
+                startDate: 'Sep 20 2019 10:00:00', 
+                endDate: 'Oct 21 2019 10:00:00'   
+            },
+            {
+                startDate: 'Oct 25 2019 10:00:00',
+                endDate: 'Nov 02 2019 10:00:00'
+            }
+        ],
+        
+        //NUMBER OF NIGHTS
+        3, 
 
-        console.log(window.location);
-    });  
+        //DEPARTURE PORTS
+        ['Fort Lauderdale', 'Miami'],
 
+        //ITINERARIES
+        null,
+
+        //PILL COLOR
+        '#110000', 
+
+        //PILL INNER TEXT
+        '50% discount on drinks' 
+    );
 });
+
+
+//exclusions type
+
 
 
