cc8ba3f4c2af0e4f841ca2065eeaab1468ea9e50
Pablo Vazquez <7006103@c02yr0jmlvcf.na.rccl.com>
2019-09-23 17:58:29 -0400

    pills reworking logic

--- js/pills.js
+++ js/pills.js
@@ -51,7 +51,7 @@
 //check if item number of nights matched pill number of nights
 //check if item departure port matches pill departure ports
 
-function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, departurePorts, itineraries, exclusions){
+function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, departurePorts, exclusions){
 
     let target = document.querySelector('.itinerary-promotions');
 
@@ -68,7 +68,9 @@ function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, 
 
     let shipCodesArr = [];
     let sailDatesArr = [];
-    let departurePortsArr = [];
+
+    //GET DEPARTURE PORTS
+    //let departurePort = document .innerText.substring(node.innerText.lastIndexOf(":")+2, node.innerText.lastIndexOf(",")).split(',');
 
     let promoDatesObjects = [];
     let sailingDatesObjects = [];
@@ -92,9 +94,6 @@ function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, 
         break;
     }
 
-    // let prevBtn = document.querySelector('.mat-paginator-navigation-previous');
-    // let nextBtn = document.querySelector('.mat-paginator-navigation-next');
-
     //GET SHIP CODES AND SAILDATES
     itineraryElements.forEach((node)=>{
         shipCodesArr.push(node.attributes.shipcode.value);
@@ -113,11 +112,6 @@ function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, 
         sailingDatesObjects.push(new Date(sail.endDate));
     });
 
-    //GET DEPARTURE PORTS
-    itineraryDescriptions.forEach((node)=>{
-        let nodeText = node.innerText.substring(node.innerText.lastIndexOf(":")+2, node.innerText.lastIndexOf(",")).split(',');
-        departurePortsArr.push(nodeText[0]);
-    });
 
     //CHECK FOR PILL REQUIREMENTS
     if(pillDetails !== null || pillDetails !== ''){
@@ -126,27 +120,19 @@ function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, 
         pillListItem.setAttribute('id', pillDetails.id);
     }
 
-    //CHECK APPLICABLE SHIPCODES
-    if(shipCodes.length !== 0 || shipCodes !== null){
-        check(shipCodesArr, shipCodes);
-    }
-
-    //CHECK APPLICABLE PROMODATES
-    if(promoDates.length !== 0 || promoDates !== null){
-        checkPromoDates(promoDatesObjects);
-    }
-
     //CHECK ALL SHIP CODES AND COMPARE
     function check(arrOne, arrTwo){
 
+        let indices = [];
         let str = arrTwo.join('');
 
         for(let i = 0; i < arrOne.length; i++){
             if(str.indexOf(arrOne[i]) !== -1){
-                console.log(i);
+                indices.push(i);
             }
         }
 
+        return indices
     }
 
     //CHECK THE PROMO DATES
@@ -154,94 +140,145 @@ function pill(pillDetails, shipCodes, promoDates, sailingDates, numberOfNights, 
 
         let currentDate = new Date();
 
-        console.log(currentDate);
-
         for(let i = 0; i < promoDatesObjects.length; i+=2){
             if(promoDatesObjects[i] < currentDate && promoDatesObjects[i+1] > currentDate){
                 console.log('good to go');
+                return true;
             }else{
                 console.log('no go');
+                return false;
             }
         }
     }
 
-    let sailDate = 
+    //CHECK THE SAILING DATE
+    function checkSailDates(sailDatesObjects, sailDate){
 
-    function checkSailDates(sailsDatesObjects, sailDate){
+        let date = sailDate.split('-');
+        let parsedDateString = new Date(date[1]+' '+date[2]+' '+date[0]+' '+'00:00:00');
 
-        
+        for(let i = 0; i < sailDatesObjects.length; i+=2){
+            if(parsedDateString > sailDatesObjects[i] && parsedDateString < sailDatesObjects[i+1]){
+                console.log('you are good to go');
+                return true;
+            }else{
+                console.log('you are not good to go');
+                return false;
+            }
+        }
+
+    }   
+
+    //CHECK THE DEPARTURE PORTS
+    function checkDeparturePorts(departurePorts, departurePort){
+
+        for(let i = 0; i < departurePorts.length; i++){
+
+        }
+
+        console.log(departurePortsArr);
+        console.log(departurePorts);
 
     }
+
+    checkSailDates(sailingDatesObjects, sailDatesArr[0]);
+
+    checkDeparturePorts(departurePortsArr, departurePorts);
     
 
 }
 
 window.addEventListener('load', ()=>{
-    pill(
-        //PILL DETAILS
-        {
-            color: '#110000',
-            text: 'Buy one get one free',
-            id: 'pill_bogo'
-        },
 
-        //SHIPCODES
-        ['NV', 'EM'],
+    // pill(
+    //     //PILL DETAILS
+    //     {
+    //         color: '#110000',
+    //         text: 'Buy one get one free',
+    //         id: 'pill_bogo'
+    //     },
+
+    //     //SHIPCODES
+    //     ['NV', 'EM'],
         
-        //PROMO DATES
-        [
-            {
-                startDate: 'Sep 18 2019 10:00:00', 
-                endDate: 'Oct 22 2019 10:00:00'    
-            },
-            {
-                startDate: 'Oct 24 2019 10:00:00',
-                endDate: 'Nov 05 2019 10:00:00'
-            }
-        ],
+    //     //PROMO DATES
+    //     [
+    //         {
+    //             startDate: 'Sep 18 2019 10:00:00', 
+    //             endDate: 'Oct 22 2019 10:00:00'    
+    //         },
+    //         {
+    //             startDate: 'Oct 24 2019 10:00:00',
+    //             endDate: 'Nov 05 2019 10:00:00'
+    //         }
+    //     ],
 
-        //SAILING DATES
-        [
-            {
-                startDate: 'Sep 20 2019 10:00:00', 
-                endDate: 'Oct 21 2019 10:00:00'   
-            },
-            {
-                startDate: 'Oct 25 2019 10:00:00',
-                endDate: 'Nov 02 2019 10:00:00'
-            }
-        ],
+    //     //SAILING DATES
+    //     [
+    //         {
+    //             startDate: 'Sep 20 2019 10:00:00', 
+    //             endDate: 'Oct 27 2019 10:00:00'   
+    //         },
+    //         {
+    //             startDate: 'Oct 25 2019 10:00:00',
+    //             endDate: 'Nov 02 2019 10:00:00'
+    //         }
+    //     ],
         
-        //NUMBER OF NIGHTS
-        3, 
+    //     //NUMBER OF NIGHTS
+    //     3, 
 
-        //DEPARTURE PORTS
-        ['Fort Lauderdale', 'Miami'],
+    //     //DEPARTURE PORTS
+    //     ['Fort Lauderdale', 'Miami'],
 
-        //ITINERARIES
-        null,
+    //     //ITINERARIES
+    //     null,
 
-        //EXCLUSIONS
-        {
-            ships:['EM', 'NE', 'OA', 'AL'],
-            numberOfNights: [7, 9],
-            averageCost: '200-400', //from-to
-            departurePorts: ['Miami', 'Sidney', 'Orlando'],
-            destinationPorts: ['Mexico', 'Puerto Rico'],
-            departureDates: [
-                {
-                    startDate: 'Sep 25 2019',
-                    endDate:' Oct 13 2019'
-                },
-                {
-                    startDate:'Oct 15 2019',
-                    endDate: 'Oct 27 2019'
-                }
-            ],
-            otherPills: ['pill_ksf', 'pill_obc']
-        }
-    );
+    //     //EXCLUSIONS
+    //     {
+    //         ships:['EM', 'NE', 'OA', 'AL'],
+    //         numberOfNights: [7, 9],
+    //         averageCost: '200-400', //from-to
+    //         departurePorts: ['Miami', 'Sidney', 'Orlando'],
+    //         destinationPorts: ['Mexico', 'Puerto Rico'],
+    //         departureDates: [
+    //             {
+    //                 startDate: 'Sep 25 2019',
+    //                 endDate:' Oct 13 2019'
+    //             },
+    //             {
+    //                 startDate:'Oct 15 2019',
+    //                 endDate: 'Oct 27 2019'
+    //             }
+    //         ],
+    //         otherPills: ['pill_ksf', 'pill_obc']
+    //     }
+    // );
+
+    pills();
+
 });
+
+function pills(){
+
+    let itineraries = document.querySelectorAll('.itinerary-card-component');
+
+    itineraries.forEach((itinerary)=>{
+        console.log(itinerary.attributes.shipcode);
+        console.log(itinerary.attributes.saildate);
+        console.log(
+            itinerary.childNodes[3]
+        );
+        //console.log(itinerary.innerText);
+    });
+
+    function getShipCode(){
+        return false; 
+    }
+
+    console.log(itineraries);
+
+}
 
 
 
