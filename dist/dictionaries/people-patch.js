9f739ae1e484e0c934ea69b6b20526a3b215bc64
Pablo Vazquez <pvazquez@vandldigital.com>
2019-11-05 16:44:27 -0500

    people watching data patch

---
+++ dist/dictionaries/people-patch.js
@@ -0,0 +1,630 @@
+var peopleWatchingData = [
+    {
+        criteria:{
+            ships:['AL'],
+            departures:['Miami','Fort Lauderdale'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[20,55],
+            business:[174,223],
+            pm:[156,218]
+        }
+    },
+    {
+        criteria:{
+            ships:['HM'],
+            departures:['Orlando','Fort Lauderdale'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[20,55],
+            business:[174,223],
+            pm:[156,218]
+        }
+    },
+    {
+        criteria:{
+            ships:['OA'],
+            departures:['Orlando','Fort Lauderdale','Cape Liberty','Miami'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[20,55],
+            business:[174,223],
+            pm:[156,218]
+        }
+    },
+    {
+        criteria:{
+            ships:['OA'],
+            departures:['Cape Liberty'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[6,18],
+            business:[57,72],
+            pm:[46,68]
+        }
+    },
+    {
+        criteria:{
+            ships:['SY'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[23,63],
+            business:[200,256],
+            pm:[180,251]
+        }
+    },
+    {
+        criteria:{
+            ships:['FR'],
+            departures:['San Juan'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[4,18],
+            business:[81,104],
+            pm:[72,100]
+        }
+    },
+    {
+        criteria:{
+            ships:['ID'],
+            departures:['Fort Lauderdale'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[4,13],
+            business:[39,51],
+            pm:[38,51]
+        }
+    },
+    {
+        criteria:{
+            ships:['LB'],
+            departures:['Galveston'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[11,39],
+            business:[98,136],
+            pm:[114,134]
+        }
+    },
+    {
+        criteria:{
+            ships:['AD'],
+            departures:['Fort Lauderdale'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[7,20],
+            business:[58,75],
+            pm:[56,76]
+        }
+    },
+    {
+        criteria:{
+            ships:['AD'],
+            departures:['Cape Liberty'],
+            nights:'>=6'
+        },
+        views: {
+            am:[6,18],
+            business:[54,71],
+            pm:[44,65]
+        }
+    },
+    {
+        criteria:{
+            ships:['EX'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[7,20],
+            business:[58,75],
+            pm:[56,76]
+        }
+    },
+    {
+        criteria:{
+            ships:['NV'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[2,4],
+            business:[13,16],
+            pm:[11,15]
+        }
+    },
+    {
+        criteria:{
+            ships:['AN'],
+            departures:['Cape Liberty'],
+            nights:'>=6'
+        },
+        views: {
+            am:[6,18],
+            business:[57,72],
+            pm:[46,68]
+        }
+    },
+    {
+        criteria:{
+            ships:['BR'],
+            departures:['Tampa'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[6,16],
+            business:[55,69],
+            pm:[50,71]
+        }
+    },
+    {
+        criteria:{
+            ships:['EN'],
+            departures:['Galveston'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[11,39],
+            business:[98,136],
+            pm:[114,134]
+        }
+    },
+    {
+        criteria:{
+            ships:['GR'],
+            departures:['Baltimore'],
+            nights:'>=6'
+        },
+        views: {
+            am:[3,8],
+            business:[26,33],
+            pm:[21,31]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['MJ'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[0,1],
+            business:[2,3],
+            pm:[3,4]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['RH'],
+            departures:['Tampa'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[2,5],
+            business:[18,24],
+            pm:[17,25]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['SR'],
+            departures:['Fort Lauderdale'],
+            itinerary:['Caribbean','Perfect Day Island'],
+            nights:'>=6'
+        },
+        views: {
+            am:[2,5],
+            business:[16,20],
+            pm:[12,17]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['HM'],
+            departures:['Orlando'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[1,3],
+            business:[7,9],
+            pm:[7,9]
+        }
+    },
+    {
+        criteria:{    
+            ships:['OA'],
+            departures:['Miami','Orlando'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[1,3],
+            business:[7,9],
+            pm:[7,9]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['SY'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[1,3],
+            business:[7,9],
+            pm:[7,9]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['ID'],
+            departures:['Fort Lauderdale'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[4,13],
+            business:[39,51],
+            pm:[38,51]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['EX'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[4,13],
+            business:[39,51],
+            pm:[38,51]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['MA'],
+            departures:['Miami','Orlando'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[12,53],
+            business:[148,196],
+            pm:[147,182]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['NV'],
+            departures:['Miami'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[2,4],
+            business:[13,17],
+            pm:[14,17]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['BR'],
+            departures:['Tampa'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[6,16],
+            business:[55,69],
+            pm:[50,71]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['EN'],
+            departures:['Orlando'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[11,42],
+            business:[203,264],
+            pm:[227,260]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['EN'],
+            departures:['Galveston'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas'],
+            nights:'<=5'
+        },
+        views: {
+            am:[2,7],
+            business:[15,22],
+            pm:[18,22]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['MJ'],
+            departures:['Fort Lauderdale'],
+            itinerary:['Caribbean','Bahamas','Perfect Day Island','Cuba'],
+            nights:'<=5'
+        },
+        views: {
+            am:[9,48],
+            business:[178,232],
+            pm:[170,228]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['VI'],
+            departures:['Galveston'],
+            itinerary:['Caribbean','Perfect Day Island','Bahamas']
+        },
+        views: {
+            am:[2,7],
+            business:[15,22],
+            pm:[18,22]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['NE'],
+            departures:['Miami'],
+            itinerary:['Cuba']
+        },
+        views: {
+            am:[9,24],
+            business:[75,99],
+            pm:[75,97]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['AD','AN'],
+            departures:['Cape Liberty'],
+            itinerary:['Bermuda'],
+            nights:'<=5'
+        },
+        views: {
+            am:[5,16],
+            business:[54,71],
+            pm:[44,65]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['GR'],
+            departures:['Baltimore'],
+            itinerary:['Bermuda'],
+            nights:'<=5'
+        },
+        views: {
+            am:[3,8],
+            business:[29,36],
+            pm:[23,36]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['SR'],
+            departures:['Boston']
+        },
+        views: {
+            am:[3,8],
+            business:[29,36],
+            pm:[23,36]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['AL','OA'],
+            departures:['Barcelona'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[14,19],
+            business:[35,44],
+            pm:[31,37]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['OA'],
+            departures:['Roma'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[14,19],
+            business:[35,44],
+            pm:[31,37]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['ID','EX','AN'],
+            departures:['Southampton'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[2,3],
+            business:[5,7],
+            pm:[5,6]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['EX'],
+            departures:['Barcelona','Roma'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[3,5],
+            business:[12,16],
+            pm:[11,14]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['BR'],
+            departures:['Barcelona','console.log(checkedCriteria);terdam'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[1,2],
+            business:[4,6],
+            pm:[4,6]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['JW'],
+            departures:['Barcelona','Copenhagen','Roma','Estocolmo'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[3,5],
+            business:[12,16],
+            pm:[1,14]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['RH'],
+            departures:['Barcelona','Venice'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[6,9],
+            business:[21,27],
+            pm:[20,24]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['SR'],
+            departures:['Copenhagen','Estocolmo'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[2,3],
+            business:[5,6],
+            pm:[5,6]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['VI'],
+            departures:['Barcelona'],
+            itinerary:['Europe']
+        },
+        views: {
+            am:[2,3],
+            business:[7,9],
+            pm:[6,8]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['OV'],
+            departures:['Vancouver','Seattle'],
+            itinerary:['Alaska']
+        },
+        views: {
+            am:[2,8],
+            business:[18,26],
+            pm:[22,27]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['RD','SR'],
+            departures:['Vancouver'],
+            itinerary:['Alaska']
+        },
+        views: {
+            am:[2,8],
+            business:[14,19],
+            pm:[16,19]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['RD'],
+            departures:['Seward'],
+            itinerary:['Alaska']
+        },
+        views: {
+            am:[2,8],
+            business:[14,19],
+            pm:[16,19]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['VI'],
+            departures:['San Juan']
+        },
+        views: {
+            am:[4,18],
+            business:[18,104],
+            pm:[72,100]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['OA'],
+            departures:['Fort Lauderdale'],
+            nights:'<=5'
+        },
+        views: {
+            am:[1,3],
+            business:[7,9],
+            pm:[7,9]
+        }
+    },
+    {
+        criteria:{ 
+            ships:['VI'],
+            departures:['Miami'],
+            nights:'<=5'
+        },
+        views: {
+            am:[2,7],
+            business:[15,22],
+            pm:[18,22]
+        }
+    }
+
+];
