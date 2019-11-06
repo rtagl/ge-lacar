f3363b5edd44a49feb71f7d62d922d22f0b9688f
Pablo Vazquez <pvazquez@vandldigital.com>
2019-11-06 09:21:45 -0500

    added escape to init function loop

--- dist/src/init.js
+++ dist/src/init.js
@@ -1,12 +1,20 @@
 //THIS FUNCTION INITIALIZES THE INJECTION OF THE COMPONENT
 function init(parent, callback){
+    var counter = 0;
 
     var timer = setInterval(function() {
 
         var el = document.querySelector(parent);
-    
-        if(el == null){
-            console.log('Parent element not found, check DOM for element');
+
+        if(counter >= 20){
+            console.log('Parent element not found, check DOM for element, end of loop.');
+            stopTimer();
+            return;
+        }
+
+        if(el === null){
+            console.log(counter ,'Parent element not found, check DOM for element');
+            counter++;
         }else{
             console.log('Component injected successfully');
             stopTimer();
--- js/init.js
+++ js/init.js
@@ -1,12 +1,20 @@
 //THIS FUNCTION INITIALIZES THE INJECTION OF THE COMPONENT
 function init(parent, callback){
+    var counter = 0;
 
     var timer = setInterval(function() {
 
         var el = document.querySelector(parent);
-    
-        if(el == null){
-            console.log('Parent element not found, check DOM for element');
+
+        if(counter >= 20){
+            console.log('Parent element not found, check DOM for element, end of loop.');
+            stopTimer();
+            return;
+        }
+
+        if(el === null){
+            console.log(counter ,'Parent element not found, check DOM for element');
+            counter++;
         }else{
             console.log('Component injected successfully');
             stopTimer();
@@ -19,3 +27,14 @@ function init(parent, callback){
         clearInterval(timer);
     }
 }
+
+window.addEventListener('load', function(){
+    init('.fluggen', function(){
+        console.log(`I'm a pickle Morty, PICKLE RICK!!!!`);
+    });
+
+    setTimeout(function(){
+        document.querySelector('.overlay').innerHTML = `<div class='fluggen'></div>`;
+    },3000)
+
+});
