32f9baedd80be07b6cb493d66f954e338a79918f
Pablo Vazquez <pvazquez@vandldigital.com>
2020-01-17 12:48:22 -0500

    people-watching complete logic rework

--- js/people-watching.js
+++ js/people-watching.js
@@ -1,11 +1,198 @@
+// function peopleWatching(props){
+
+//     var createPeopleWatchingComponent = function(){
+//         var component = document.createElement('div');
+//         component.style.background = '#ff6a6f';
+//         component.style.width = '100%';
+//         component.style.height = '18px';
+//         component.style.padding = '7px 0px';
+//         component.style.margin = '-162px 0px 0px 0px';
+//         component.style.zIndex = 999;
+//         component.style.display = 'flex';
+//         component.style.justifyContent = 'center';
+//         component.style.alignItems = 'center';
+//         component.setAttribute('id', 'ge_people-watching');
+
+//         var eyeSpan = document.createElement('img');
+//         eyeSpan.style.width = '20px';
+//         eyeSpan.style.height = '20px';
+//         eyeSpan.style.margin = '0px 5px 0px 5px';
+//         eyeSpan.setAttribute('src', 'https://sb.monetate.net/img/1/388/1982062.png');
+//         eyeSpan.style.cssFloat = 'left';
+
+//         var numberSpan = document.createElement('span');
+//         numberSpan.style.width = 'auto';
+//         numberSpan.style.height = 'auto';
+//         numberSpan.style.textAlign = 'center';
+//         numberSpan.style.fontFamily = 'ProximaNova-Bold, Helvetica';
+//         numberSpan.style.fontSize = '18px';
+//         numberSpan.style.margin = '0px 3px 0px 3px';
+//         numberSpan.style.padding = '0px';
+//         numberSpan.style.color = 'white';
+//         numberSpan.style.cssFloat = 'left';
+//         numberSpan.innerText = '100';
+
+//         var peopleSpan = document.createElement('span');
+//         peopleSpan.style.width = 'auto';
+//         peopleSpan.style.height = 'auto';
+//         peopleSpan.style.textAlign = 'center';
+//         peopleSpan.style.fontFamily = 'ProximaNova-Bold, Helvetica';
+//         peopleSpan.style.fontSize = '18px';
+//         peopleSpan.style.margin = '0px 3px 0px 3px';
+//         peopleSpan.style.color = 'white';
+//         peopleSpan.innerText = props.people;
+//         peopleSpan.style.cssFloat = 'left';
+//         peopleSpan.style.textTransform = 'capitalize';
+
+//         var  watchingSpan = document.createElement('span');
+//         watchingSpan.style.width = 'auto';
+//         watchingSpan.style.height = 'auto';
+//         watchingSpan.style.textAlign = 'center';
+//         watchingSpan.style.fontFamily = 'ProximaNova-Regular, Helvetica';
+//         watchingSpan.style.fontSize = '18px';
+//         watchingSpan.style.margin = '0px 3px 0px 3px';
+//         watchingSpan.style.color = 'white';
+//         watchingSpan.innerText = props.watching;
+//         watchingSpan.style.cssFloat = 'left';
+//         watchingSpan.style.textTransform = 'capitalize';
+
+//         component.appendChild(eyeSpan);
+//         component.appendChild(numberSpan);
+//         component.appendChild(peopleSpan);
+//         component.appendChild(watchingSpan);
+
+//         return {
+//             component: component,
+//             eyeSpan: eyeSpan,
+//             numberSpan: numberSpan,
+//             peopleSpan: peopleSpan,
+//             watchingSpan: watchingSpan,
+//         }
+//     }
+
+//     var adjustSidePanelElements = function(){
+//         document.querySelectorAll('.itinerary-panel-section').forEach(function(section){
+//             section.style.marginTop = '20px';
+//         });
+
+//         document.querySelectorAll('.itinerary-panel-details-section').forEach(function(detail){
+//             detail.style.marginTop = '20px';
+//         });
+
+//         document.querySelectorAll('.mat-button-wrapper')[1].style.marginTop = '20px';
+//     }
+
+//     var show = function(element){
+//         if(element.style.marginTop === '-130px'){
+//             element.style.marginTop = '-162px'
+//         }
+//         if(element.style.marginTop === '-162px'){
+//             setTimeout(function(){
+//                 var timer = setInterval(function(){
+//                     if(element.style.marginTop === '-130px'){
+//                         clearInterval(timer);
+//                     }else{
+//                         element.style.marginTop = parseInt(element.style.marginTop) + 16 + 'px';
+//                     }
+//                 }, 50);
+//             }, 300);
+//         }
+//     }
+
+//     var hide = function(element){
+//         element.style.margin = '-130px 0px 0px 0px';
+//     }
+
+//     var updateItinerearyClicks = function(id){
+//         var click = state.clicks.find(function(click){
+//             return click.id === id;
+//         });
+
+//         if(click){
+//             click.count ++;
+//            if((Math.floor(Math.random() * (5 - 1 + 1) + 1)) === 5){
+//              click.peopleWatching += Math.floor(Math.random() * (10 - 3 + 1) + 3);
+//            }else if((Math.floor(Math.random() * (5 - 1 + 1) + 1)) === 1){
+//              click.peopleWatching -= Math.floor(Math.random() * (7 - 3 + 1) + 3);
+//            }
+//         }else{
+//             state.clicks.push({
+//                 count: 1,
+//                 id: id,
+//                 peopleWatching: Math.floor(Math.random() * (100 - 60 + 1) + 60),
+//             });
+//         }
+//     }
+
+//     var getSidePanel = function(){
+//         return document.querySelector('.itinerary-panel-sidenav');
+//     }
+
+//     var updatePeopleWatchingNumber = function(id){
+//         var click = state.clicks.find(function(click){
+//             return click.id === id;
+//         });
+
+//         state.components.numberSpan.innerText = click.peopleWatching;
+//     }
+
+//     var toggleVisibility = function(cbShow, cbHide, element){
+//         document.addEventListener('click', function(){
+//             for(var i = 0; i < state.itineraries.length; i++){
+//                 if(window.location.href.indexOf(state.itineraries[i]) !== -1){
+//                     cbShow(element);
+//                     break;
+//                 }
+//                 if(window.location.href.indexOf(state.itineraries[i]) === -1){
+//                     cbHide(element);
+//                 }
+//             }
+//         });
+//     }
+
+//     var addPeopleWatchinComponent = function(){
+//         if(!document.querySelector('ge_people-watching')){
+//             state.sidePanel.appendChild(state.components.component);
+//         }
+//     }
+
+//     var state = {
+//         clicks:[],
+//         itineraries: [],
+//         components: createPeopleWatchingComponent(),
+//         sidePanel: getSidePanel()
+//     }
+
+//     function main(){
+
+//     }
+
+//     main();
+
+// }
+
+// peopleWatching({
+//     people: 'people',
+//     watching: 'watching'
+// });
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     peopleWatching({
+//         people: 'people',
+//         watching: 'watching'
+//     });
+// })
+
 function peopleWatching(props){
 
     var state = {
         clicks:[],
-        itineraries: []
+        itineraries: [],
+        components: createPeopleWatchingComponent(),
+        sidePanel: getSidePanel()
     }
 
-    var createPeopleWatchingComponent = function(){
+    function createPeopleWatchingComponent(){
         var component = document.createElement('div');
         component.style.background = '#ff6a6f';
         component.style.width = '100%';
@@ -75,7 +262,11 @@ function peopleWatching(props){
         }
     }
 
-    var adjustSidePanelElements = function(){
+    function getSidePanel (){
+        return document.querySelector('.itinerary-panel-sidenav');
+    }
+
+    function adjustSidePanelElements(){
         document.querySelectorAll('.itinerary-panel-section').forEach(function(section){
             section.style.marginTop = '20px';
         });
@@ -87,7 +278,7 @@ function peopleWatching(props){
         document.querySelectorAll('.mat-button-wrapper')[1].style.marginTop = '20px';
     }
 
-    var show = function(element){
+    function show(element){
         if(element.style.marginTop === '-130px'){
             element.style.marginTop = '-162px'
         }
@@ -104,43 +295,19 @@ function peopleWatching(props){
         }
     }
 
-    var hide = function(element){
+    function hide(element){
         element.style.margin = '-130px 0px 0px 0px';
     }
 
-    var getItineraries = function(){
-        if(state.itineraries.length === 0){
-
-            document.querySelectorAll('li.collapsable').forEach(function(itinerary){
-                state.itineraries.push(itinerary.attributes.id.value);
-            });
-
-        }else if(state.itineraries.length !== 0){
-            
-            var itineraries = [];
-            document.querySelectorAll('li.collapsable').forEach(function(itinerary){
-                itineraries.push(itinerary.attributes.id.value);
-            });
-            
-            console.log(itineraries.join());
-            console.log(state,itineraries.join());
-            
-            itineraries.join() !== state.itineraries.join() ? state.itineraries = itineraries : null ;
-
-        }
-        return document.querySelectorAll('li.collapsable');
-    }
-
-    var addClickEventsToItinerary = function(cbUpdateClicks, cbUpdateNumberOfPeople){
-        state.itineraries.forEach(function(itinerary){
-            document.getElementById(itinerary).addEventListener('click', function(){
-                cbUpdateClicks(itinerary);
-                cbUpdateNumberOfPeople(itinerary);
-            })
+    function updatePeopleWatchingNumber(id){
+        var click = state.clicks.find(function(click){
+            return click.id === id;
         });
+
+        state.components.numberSpan.innerText = click.peopleWatching;
     }
 
-    var updateItinerearyClicks = function(id){
+    function updateItinerearyClicks(id){
         var click = state.clicks.find(function(click){
             return click.id === id;
         });
@@ -161,117 +328,37 @@ function peopleWatching(props){
         }
     }
 
-    var handlePaginationsClicks = function(){
-        document.onclick = function(e){
-            if( e.target.dataset.selector === 'search-apply-button' ||
-                e.target.innerText === 'apply' ||
-                e.target.innerText === 'add' ||
-                e.target.innerText === 'clear' ||
-                e.target.dataset.selector === 'search-2to5-nights' ||
-                e.target.dataset.selector === 'search-6to8-nights' ||
-                e.target.dataset.selector === 'search-9to11-nights' ||
-                e.target.dataset.selector === 'search-12plus-nights' ||
-                e.target.classList.value.indexOf('mat-button-toggle-label-content') !== -1 ||
-                e.target.classList.value.indexOf('mat-paginator-decrement') !== -1 ||
-                e.target.classList.value.indexOf('mat-paginator-increment') !== -1 ||
-                e.target.classList.value.indexOf('mat-paginator-navigation-next') !== -1 ||
-                e.target.classList.value.indexOf('mat-paginator-navigation-previous') !== -1
-            ){  
-                console.log('NAV LINK FOR PAGINATION CLICKED!!!!!!!!');
-                return true;
-            }
+    function checkUrl(){
+        var url = window.location.href
+        if(url.indexOf('itineraryPanel') !== -1){
+            console.log(url.substr(url.indexOf('itineraryPanel')+'itineraryPanel'.length + 1, url.length - 1));
+            return {status: true, id: url.substr(url.indexOf('itineraryPanel')+'itineraryPanel'.length + 1, url.length - 1)}
+        }else{
+            return {status: false}
         }
     }
-
-    // var updateItineraries = function(){
-
-    //     var itineraries = document.querySelectorAll('li.collapsable');
-
-    //     var itineraryIDs = [];
-    //     itineraries.forEach(function(itinerary){
-    //         itineraryIDs.push(itinerary.attributes.id.value);
-    //     });
-
-    //     console.log(state.itineraries);
-
-    //     if(state.itineraries.length !== 0 && itineraryIDs.join() === state.itineraries.join()){
-    //         return false;
-    //     }else{
-    //         return true;
-    //     }
-
-    // }
-
-    var getSidePanel = function(){
-        return document.querySelector('.itinerary-panel-sidenav');
-    }
-
-    var updatePeopleWatchingNumber = function(id){
-        var click = state.clicks.find(function(click){
-            return click.id === id;
-        });
-
-        return click.peopleWatching;
-    }
-
-    var checkURL = function(cbShow, cbHide, element){
-
-        var lis = document.querySelectorAll('li.collapsable');
-        var ids = [];
     
-        lis.forEach(function(li){
-            ids.push(li.id);
-        });
-
-        document.addEventListener('click', function(){
-            for(var i = 0; i < ids.length; i++){
-                if(window.location.href.indexOf(ids[i]) !== -1){
-                    cbShow(element);
-                    break;
-                }
-                if(window.location.href.indexOf(ids[i]) === -1){
-                    cbHide(element);
-                }
-            }
-        });
-    
-    }
-
     function main(){
+        document.onclick = function(){
+            var check  = checkUrl();
 
-        // check page is cruises
-            // get itineraries off page
-            // add clicks to itineraries
-            // listen to clicks on pagination and filter buttons
-                // get itineraries off page
-                // check if itineraries are different from stored itineraries
-                // update stored itineraries
-                // add clicks to new itineraries
-            // add component to sidepanel
-            // update people watching number
-
-
-        if(window.location.href.indexOf('cruises') !== -1){
-
-            var itineraries = getItineraries();
-            var peopleWatchingComponent = createPeopleWatchingComponent();
-            var sidePanel = getSidePanel();
-
-            var pageClicks = handlePaginationsClicks();
-
-            console.log('kjadvklja,kjnadsdiuhqglkjndfgad', pageClicks);
-
-            if(pageClicks === true){
-                getItineraries();
-                addClickEventsToItinerary();
+            if(check.status === true){
+                //update number of clicks
+                updateItinerearyClicks(check.id);
+                //add component to panel
+                state.sidePanel.appendChild(state.components.component);
+                //adjust side panel elements
+                adjustSidePanelElements();
+                //show component
+                show(state.components.component);
+                //update numner of people
+                updatePeopleWatchingNumber(check.id);
+            }else{
+                hide(state.components.component)
             }
-
-            sidePanel.appendChild(peopleWatchingComponent.component);
-            checkURL(show, hide, peopleWatchingComponent.component);
-
         }
     }
-
+    
     main();
 
 }
@@ -280,11 +367,3 @@ peopleWatching({
     people: 'people',
     watching: 'watching'
 });
-
-window.addEventListener('DOMContentLoaded', function(){
-    peopleWatching({
-        people: 'people',
-        watching: 'watching'
-    });
-})
-
